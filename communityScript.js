eb45ad9c4692d3e761fc5061d1f446a773784cb4
mccauleye1 <mccauleye1@mymail.nku.edu>
2025-12-03 22:39:31 -0500

    Update communityScript.js

--- communityScript.js
+++ communityScript.js
@@ -3,6 +3,7 @@ document.getElementById("switchpage").addEventListener("change", function () {
     const page = this.value;
     if (page) {
         window.location.href = page;
+        this.value = page;
     }
 });
 
